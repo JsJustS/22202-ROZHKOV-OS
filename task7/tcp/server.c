75ff97a2ecbebc2dca0c690a6a2b1a9165b49d3a
jsJustS <JustS.js@yandex.ru>
2024-07-12 19:47:01 +0300

    rewrite

--- task7/tcp/server.c
+++ task7/tcp/server.c
@@ -9,39 +9,33 @@
 
 #define PORT 8888
 #define sockaddr struct sockaddr_in
-const int MAX_CLIENTS = 5;
+#define BUFSIZE 4096
 
 int serveClient(int clt_sock);
 
 void main() {
 	int srv_sock;
+	int clt_sock;
 	int client_count = 0;
-	int clt_socks[MAX_CLIENTS];
-	int clt_pids[MAX_CLIENTS];
 	int i;
-	for (i = 0; i < MAX_CLIENTS; ++i) {
-		clt_socks[i] = -1;
-		clt_pids[i] = -1;
-	}
-
-	sockaddr srv_sockaddr;
-	int len = 0;
-	int val = 0;
 	int err;
+	sockaddr srv_sockaddr;
+	sockaddr clt_sockaddr;
 
 	// Init server
-	srv_sock = socket(AF_INET, SOCK_STREAM | O_NONBLOCK, 0);
+	srv_sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (srv_sock == -1) {
 		perror("Failed on creating server socket");
 		return;
 	}
+
 	int option = 1;
 	setsockopt(srv_sock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
 	memset(&srv_sockaddr, 0, sizeof(sockaddr));
 	srv_sockaddr.sin_family = AF_INET;
 	srv_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    	srv_sockaddr.sin_port = htons(PORT);
+	srv_sockaddr.sin_port = htons(PORT);
 	
 	err = bind(srv_sock, (sockaddr*) &srv_sockaddr, sizeof(srv_sockaddr));
 	if (err == -1) {
@@ -50,7 +44,7 @@ void main() {
 		return;
 	}
 
-	err = listen(srv_sock, MAX_CLIENTS);
+	err = listen(srv_sock, 5);
 	if (err == -1) {
 		perror("Could not switch server socket to passive mode");
 		close(srv_sock);
@@ -59,106 +53,58 @@ void main() {
 
 	printf("Waiting for conections on 127.0.0.1:%d\n", PORT);
 
-	int running = 1;
-	while (running) {
-		for (i = 0; i < MAX_CLIENTS; ++i) {
-			if (clt_socks[i] != -1) {
-				int status;
-				int res = waitpid(clt_pids[i], &status, WNOHANG);
-				if (res == -1) {
-					perror("fail");
-					return;
-				} else if (res > 0) {
-					close(clt_socks[i]);
-					clt_socks[i] = -1;
-					clt_pids[i] = -1;
-					client_count--;
-					printf("Client left: %d/%d.\n", client_count, MAX_CLIENTS);
-					if (WEXITSTATUS(status) == 1) {
-						puts("Server closed...");
-						running = 0;
-					}
-				}
-			}
+	int len = 0;
+	while (1) {
+		int clt_sock = accept(srv_sock, (sockaddr *) &clt_sockaddr, &len);
+
+		if (clt_sock == -1) {
+			perror("Could not accept client");
+			close(srv_sock);
+			return;
 		}
 
-		if (client_count < MAX_CLIENTS) {
-			sockaddr clt_sockaddr;
-			memset(&clt_sockaddr, 0, sizeof(sockaddr));
-			int clt_sock = accept4(srv_sock, (sockaddr*) &clt_sockaddr, &len, O_NONBLOCK);
+		int pid = fork();
+		if (pid == -1) {
+			perror("Could not fork");
+			close(srv_sock);
+			return;
+		}
 
-			// Не смогли установить новое соединение
-			if (clt_sock == -1) {
-				if (errno == EWOULDBLOCK) {
-					// Просто новых клиентов нет
-        				puts("...");
-        				sleep(1);
-					continue;
-      				}
-				// Иначе произошла какая-то ошибка
-				perror("Could not accept client");
-				close(srv_sock);
-				for (i = 0; i < MAX_CLIENTS; ++i) {
-					if (clt_socks[i]!=-1) {
-						close(clt_socks[i]);
-						int st;
-						wait(&st);
-					}
-				}
-				return;
-			}
-			// Новый клиент подключился
-			client_count++;
-			printf("Client connected: %d/%d.\n", client_count, MAX_CLIENTS);
-			for (i = 0; i < MAX_CLIENTS; ++i) {
-				if (clt_socks[i] == -1) break;
-			}
-			clt_socks[i] = clt_sock;
-			int clt_pid = fork();			
-			if (clt_pid == 0) {
-				int exitcode = serveClient(clt_sock);
-				close(clt_sock);
-				exit(exitcode);
-			} else if (clt_pid < 0) {
-				perror("Could not fork for client");
-				close(clt_sock);
-				for (i = 0; i < MAX_CLIENTS; ++i) {
-					if (clt_socks[i]!=-1) {
-						close(clt_socks[i]);
-						int st;
-						wait(&st);
-					}
-				}
-				return;
-			} else {
-				clt_pids[i] = clt_pid;
-			}
+		// fork() == 0: child, serves client, does not need server fd
+		// fork() != 0: parent, accepts new clients untill death
+		if (pid == 0) {
+			close(srv_sock);
+			serveClient(clt_sock);
+			return;
 		}
 	}
 }
 
 int serveClient(int clt_sock) {
-	char data[4096];
+	char data[BUFSIZE + 1];
+	int status_code;
 	while (1) {
 		int ret;
-		ret = read(clt_sock, data, sizeof(data));
-		if (ret == -1) continue;
-		if (ret == 0) {
-			printf("Client %d died.\n", getpid());
+		ret = read(clt_sock, data, BUFSIZE);
+		if (ret == -1) {
+			perror("Could not read data from client.");
+			status_code = -1;
 			break;
 		}
-		data[ret] = '\0';
-		if (write(clt_sock, data, ret) < 1) {
-			perror("Could not write data to client.");
-			return 0;
+		if (ret == 0) {
+			printf("Client %d closed.\n", getpid());
+			status_code = 0;
+			break;
 		}
 
-		puts(data);
-		if (strcmp(data, "exit")==0) {
-			return 0;
-		} else if (strcmp(data, "close")==0) {
-			return 1;
+		data[BUFSIZE] = '\0';
+		printf("%d > %s\n", getpid(), data);
+
+		if (write(clt_sock, data, BUFSIZE) < 1) {
+			perror("Could not write data to client.");
+			status_code = -1;
+			break;
 		}
 	}
-	return -1;
+	return status_code;
 }
